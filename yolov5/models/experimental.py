414eb591527194ad7384e48073c9bf9f30fc97db
abnaveira.dev <abnaveira.dev@gmail.com>
2021-06-17 22:11:39 +0200

    change import

--- yolov5/models/experimental.py
+++ yolov5/models/experimental.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 
 from yolov5.models.common import Conv, DWConv
+from yolov5.utils.general import yolov5_in_syspath
 from yolov5.utils.google_utils import attempt_download
 
 
