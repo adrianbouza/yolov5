03d56b61299c17567fbcb2b2671a6f09f5af4279
abnaveira.dev <abnaveira.dev@gmail.com>
2021-06-17 14:38:07 +0200

    add setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
 NAME = "yolov5"
 DESCRIPTION = "YOLOV5 object detector"
-URL = "https://github.com/adrianbouza/EfficientDet"
+URL = "https://github.com/adrianbouza/yolov5"
 VERSION = "1.0.0"
 setup(
     name=NAME,
